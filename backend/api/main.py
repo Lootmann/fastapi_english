c66672f9cf89ba1b3b64075e17ffa6341e14d3a6
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-07-20 22:01:16 +0900

    routers: examples

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
+from api.routers.examples import router as example_router
 from api.routers.words import router as word_router
 
 app = FastAPI()
 
 app.include_router(word_router)
+app.include_router(example_router)
